2e185c5dd001d9c5d426cd5efba0059b9227ca80
Saul A. <54ulxd@gmail.com>
2023-12-12 21:25:48 +0000

    [FEAT] added dragon and directional light test

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public/')));
